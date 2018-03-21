ec0d163335c995abfdafc2f951e78ee9d78410a3
epajic16747 <epajic2@etf.unsa.ba>
2018-03-21 13:27:03 +0100

    Projekti

---
+++ Autoprevoznik/src/main/java/Modul/Autoprevoznik/App.java
@@ -0,0 +1,13 @@
+package Modul.Autoprevoznik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
