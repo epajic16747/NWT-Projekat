5b49b74cdfb1abd195dd5695231d4433eb831ace
epajic16747 <epajic2@etf.unsa.ba>
2018-03-26 19:39:40 +0200

    Pocetni projekti v2

---
+++ KorisnikRecenzija/src/main/java/Modul/KorisnikRecenzija/App.java
@@ -0,0 +1,13 @@
+package Modul.KorisnikRecenzija;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
