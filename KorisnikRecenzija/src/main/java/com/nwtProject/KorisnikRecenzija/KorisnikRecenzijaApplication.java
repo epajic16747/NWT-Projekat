0c361fec0339c60491a5515547c6935b28170b7f
epajic16747 <epajic2@etf.unsa.ba>
2018-04-10 01:20:50 +0200

    Eureka Serice Registry i Klijent za KorisnikRecenzijaModul faza 1

--- KorisnikRecenzija/src/main/java/com/nwtProject/KorisnikRecenzija/KorisnikRecenzijaApplication.java
+++ KorisnikRecenzija/src/main/java/com/nwtProject/KorisnikRecenzija/KorisnikRecenzijaApplication.java
@@ -7,6 +7,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
 
 import com.nwtProject.KorisnikRecenzija.Models.Korisnik;
 import com.nwtProject.KorisnikRecenzija.Models.Recenzija;
@@ -21,6 +34,21 @@ public class KorisnikRecenzijaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KorisnikRecenzijaApplication.class, args);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	/*
 	 * 
 	 Projektni zadatak 1
@@ -101,4 +129,20 @@ public class KorisnikRecenzijaApplication {
 			log.info("");
 		};
 	}	*/
+}
+
+
+//Eureka service client 
+
+@RestController
+class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
 }
