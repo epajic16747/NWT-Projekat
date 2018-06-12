c046d2eef62e9b526ee81b566ed1249a2a63c71c
epajic16747 <epajic2@etf.unsa.ba>
2018-06-12 05:15:02 +0200

    Part2

---
+++ AuthorizationClient/src/main/java/com/nwtProject/AuthorizationClient/AuthorizationClientApplication.java
@@ -0,0 +1,12 @@
+package com.nwtProject.AuthorizationClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationClientApplication.class, args);
+	}
+}
